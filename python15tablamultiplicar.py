b15aceb7ba5a66c4e0d5ff5740873ad93c939b9d
“anasob-creator” <sobrini.ana@gmail.com>
2026-01-26 17:43:19 +0100

    commit1

---
+++ python15tablamultiplicar.py
@@ -0,0 +1,16 @@
+# Pedimos el número al usuario y el programa nos muestra la tabla de multiplicar de ese número del 1 al 10
+print("#########Tabla de multiplicar##########")
+# utilizaremos un bucle for porque sabemos el número de repeticiones. Pedimos el número
+numero=int(input("Dame un número para mostrar su tabla de multiplicar: "))
+print("Tabla de multiplicar del número ", numero)
+total=1
+for i in range(1, 10+1):
+    # multiplicamos
+    total=int(numero*i)
+    # mostramos el resultado
+    print(total)
+    #sumamos a la variable total
+    total=total+1
+print("Fin del programa")
+  
+
