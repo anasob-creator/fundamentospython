cc18f39678fe4df96ed98a8959ef143918b70c16
“anasob-creator” <sobrini.ana@gmail.com>
2026-01-26 17:13:50 +0100

    rename

---
+++ python13conjeturadecollatz.py
@@ -0,0 +1,23 @@
+####### Conjetura de Collatz #######
+# Todo número entero positivo siempres llegará a ser 1 siguiendo dos concidiones:
+# 1. Si el número es par se divide por 2
+# 2. Si el número es impar se multiplica por 3 y se le suma 1
+# Ejemplo: 6 es par, 6/2=3, 3 es impar, 3*3+1=10, 10 es par, 10/2=5, 5 es impar, 5*3+1=16, 16 es par, 16/2=8, 8 es par, 8/2=4, 4 es par, 4/2=2, 2 es par, 2/2=1
+# El programa pedirá un número y mostrará la secuencia hasta llegar a 1
+# No se puede hacer con un for, porque no sabemos el número de repeticiones, usaremos un while
+print("#########Conjetura de Collatz##########")
+numero=int(input("Dame un número: "))   
+ # Pedimos el número al usuario
+print("Secuencia de Collatz para el número ", numero)
+ # Introducimos while y le ponemos la condición de salida,que será cuando el número sea diferente de 1.
+while (numero!=1):
+    print(numero)
+    if (numero%2==0):
+        # el número par
+        numero=int(numero/2)
+    else:
+        # número impar
+        numero=(numero*3)+1
+print("Fin del programa")
+
+
