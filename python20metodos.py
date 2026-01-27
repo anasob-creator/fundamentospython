afc1f17b580aa309f114a111073f2999cfbfbcea
“anasob-creator” <sobrini.ana@gmail.com>
2026-01-27 18:07:14 +0100

    nuevo

---
+++ python20metodos.py
@@ -0,0 +1,31 @@
+####### FUNCIONES / METODOS DE PYTHON ##############
+# Una función es un método y un método es una función, es lo mismo
+# Son herramientas que contienen los programas de Python y sus clases también
+# Un método de una clase de Python: (string) (upper())
+# Una función de Python: len(texto)
+# Cualquier programa se ejecuta y utiliza funciones/métodos de Python
+# Los métodos o funciones hacen que nuestro código no sea lineal, además podemos reutilizar las funciones
+# Actualmente hemos validado un email, dni o un isbn.
+# Si queremos validar un email, usaremos el método que hayamos creado
+# El código lo separaremos en diferentes programas. 
+# Tendremos programas con métodos con ejecución/llamadas de dichos métodos
+# También podremos tener un mismo programa, métodos y también la parte principal de la ejecución
+# Por un lado nuestro programa principal es Python, será el mail
+####### NUestro programa en Python (MAIN)
+# print("Validación de email")
+# print("Dame un email")
+# email=input()
+#LLamada a un método o función (en lugar de los if, elif.....)
+# me lo invento: validaremail()
+###### En otro lugar de nuestro CODIGO/FICHERO
+# En otro lugar tendremos la llamada:
+# # validaremail(emailrecibido):
+# # #Aquí Comprobamos las cosas
+# En python para declarar métodos se utiliza la palabra clave def y a continuación el nombre del método.
+# Los métodos pueden estar en el mismo fichero o en otro
+# Si están en el mismo lo primero que se escriben son dichos métodos.
+# Los métodos o funciones pueden recibir parámetros
+# (el email recibido es un parámetro.)
+# SINTAXIS
+
+
