5411baa71184a8227080cdea7145fdcb4d69f6c3
“anasob-creator” <sobrini.ana@gmail.com>
2026-01-27 20:21:49 +0100

    nuevo

---
+++ python23calculadorabucle.py
@@ -0,0 +1,50 @@
+# Versión 2 de la calculadora
+# Tiene que hacer un bucle para que el usuario hasta que el usuario seleccione la opción que yo desee o hasta que quiera salir
+# En el main
+# Pediremos dos números al usuario
+# Mostraremos un menú con las siguientes opciones
+# 1.- Sumar
+# 2.- Restar
+# 3.- Multiplicar
+# Tendremos cuatro funciones, tres return con las operaciones y otra para mostrar el menú
+# Primero creamos los métodos
+#Sumar
+def sumar(num1,num2):
+    suma=num1+num2
+    return suma
+#restar. Se puede hacer sin guardar también
+def restar(num1,num2):
+    return num1-num2
+def multiplicar(num1,num2):
+    return num1*num2
+# un método de acción
+def mostrarMenu():
+    print("0. Salir")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("Seleccione una opción: ")
+# Aquí hacemos el programa MAIN
+print("Ejemplos de método return")
+print("Introduce un número: ")
+numero1=int(input())
+print("Introduce otro número: ")
+numero2=int(input())
+# Quiero mostrar el menú hasta que elija salir
+# CUando el usuario elija el 0 salimos del bucle
+# primero le damos valor a opción para que entre en el bucle
+opcion=-1
+while(opcion!=0):
+    mostrarMenu()
+    opcion=int(input())
+    resultado=0
+    if(opcion==1):
+        resultado=sumar(numero1,numero2)
+    elif(opcion==2):
+        resultado=restar(numero1,numero2)
+    elif(opcion==3):
+        resultado=multiplicar(numero1,numero2)
+    else:
+        print("opción no válida")
+        print("Operación:", resultado)
+        print("Fin del programa")
