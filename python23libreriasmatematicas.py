fcbe224fea796b37d89f7dc7a6bd0029d6b55037
“anasob-creator” <sobrini.ana@gmail.com>
2026-01-29 16:40:15 +0100

    cambio

---
+++ python23libreriasmatematicas.py
@@ -0,0 +1,23 @@
+######## LIBRERÍAS EN PYTHON ########
+# Python tiene la libería math
+import math
+from math import trunc, floor
+# Acabamos de crear un ejemplo con un programa y métodos en el mismo fichero
+# Ahora mismo cada vez que yo quiera utilizar getNumero, necesitamos copiar el método de getNumero() a otro programa
+# El concepto de librería está en separar los métodos de los programas. 
+# Tendremos un fichero con todos los def, será una librería de métodos
+# LIBRERIAS
+# def
+
+# main
+# from librerias import metodo1() metodo2()
+#
+#
+#print ("Bienvenido a mi programa")
+#dato=input()
+#metodo1()
+# losnombres de librerías serán losnombres de los ficheros de python
+# En nuestro programa trabajaremos así a partir de ahora
+# liberíaXX: la clase con los métodos def
+# pythonXX: Esto será nuestro programa Main/Program de ejecución
+# Lo primero será llevarnos nuestro ejempl oa este nuevo programa a 
