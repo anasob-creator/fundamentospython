6176c85c31e29694d845e0fe1c1caf316d00b8f6
“anasob-creator” <sobrini.ana@gmail.com>
2026-01-29 17:33:49 +0100

    cambios

--- python22calculadoraversion3.py
+++ python22calculadoraversion3.py
@@ -9,59 +9,62 @@
 # Tendremos cuatro funciones, tres return con las operaciones y otra para mostrar el menú
 # Primero creamos los métodos
 #Sumar
-def sumar(num1,num2):
-    suma=num1+num2
+def sumarNumeros(num1, num2):
+    suma = num1 + num2
     return suma
-#restar. Se puede hacer sin guardar también
-def restar(num1,num2):
-    return num1-num2
-def multiplicar(num1,num2):
-    return num1*num2
-# un método de acción
-def mostrarMenu():
-    print("0. Salir")
-    print("1. Sumar")
-    print("2. Restar")
-    print("3. Multiplicar")
-    print("Introducir nuevos números: ")
-    print("Seleccione una opción: ")
-#Creamos un método que devolverá el método introducido, con un bucle infinito hasta que nos de un número
+def restarNumeros(num1, num2):
+    return num1 - num2
+def multiplicarNumeros(num1, num2):
+    return num1 * num2
+def menuCalculadora():
+    print("0.- Salir")
+    print("1.- Sumar")
+    print("2.- Restar")
+    print("3.- Multiplicar")
+    print("4.- Introducir nuevos números")
+    print("Selecciona una opción")
+
+#Creamos un metodo que devolverá el número introducido
+#En este método, tendremos un bucle infinito hasta que
+#nos de un número
 def getNumero():
-    print("Introduzca su número: ")
-    #  almacenamos lo que escriba el usuario en una variable string
-    aux=input()
-    # Entramos en un bucle mientras no sean números
-    while(aux.isdigit()==False):
-        print("Eso no es un número")
-        print("Introducir nuevos números: ")
-        aux=input()
-    #convertimos el texto a número
-    num=int(aux)
-    # Devolvemos en número de este método
+    print("Introduzca un número")
+    #ALMACENAMOS LO QUE ESCRIBA EL USUARIO
+    #EN UNA VARIABLE string
+    aux = input()
+    #Entramos en un bucle mientras que NO sean NUMEROS
+    while (aux.isdigit() == False):
+        print("Eso no era un numero")
+        print("Introduzca número")
+        aux = input()
+    #Convertimos el texto a numero
+    num = int(aux)
+    #Devolvemos el numero desde el metodo
     return num
-# Aquí hacemos el programa MAIN
-print("Ejemplos de método return versión 3")
-# Aquí metemos el método que recoge el número
-numero1=getNumero()
-numero2=getNumero()
-# Quiero mostrar el menú hasta que elija salir
-# CUando el usuario elija el 0 salimos del bucle
-# primero le damos valor a opción para que entre en el bucle
-opcion=-1
-while(opcion!=0):
-    mostrarMenu()
-    opcion=int(input())
-    resultado=0
-    if(opcion==1):
-        resultado=sumar(numero1,numero2)
-    elif(opcion==2):
-        resultado=restar(numero1,numero2)
-    elif(opcion==3):
-        resultado=multiplicar(numero1,numero2)
-    elif(opcion==4):
-        numero1=getNumero()
-        numero2=getNumero()
+        
+#Programa principal MAIN
+print("Programa calculadora")
+numero1 = getNumero()
+numero2 = getNumero()
+#Quiero mostrar el menu calculadora, hasta cuando???
+#Cuando el usuario ponga SALIR ( 0 ), salimos del bucle
+#Vamos a dar un valor por defecto a opcion para que 
+#entre en el bucle
+opcion = -1
+while (opcion != 0):
+    menuCalculadora()
+    opcion = int(input())
+    operacion = 0
+    if (opcion == 1):
+        operacion = sumarNumeros(numero1, numero2)
+    elif (opcion == 2):
+        operacion = restarNumeros(numero1, numero2)
+    elif (opcion == 3):
+        operacion = multiplicarNumeros(numero1, numero2)
+    elif (opcion == 4):
+        numero1 = getNumero()
+        numero2 = getNumero()
     else:
-        print("opción no válida")
-        print("Operación:", resultado)
-        print("Fin del programa")
+        print("Opción no valida")
+    print("Operacion: ", operacion)
+print("Fin de programa")
