a3ba59059db89839b4a906633c25e0821e6d7b4a
“anasob-creator” <sobrini.ana@gmail.com>
2026-01-29 17:33:58 +0100

    cambios

--- python23matematicas.py
+++ python23matematicas.py
@@ -1,8 +1,7 @@
-# # # Programa para ejecutar con la librería de matemáticas # # # 
-# Llamamos a la librería de matemáticas
+#Llamamos a la librería de matemáticas
 import libreria23matematicas
 print("Calculadora de mates")
-numero1=5
-numero2=9
-# Llamamos a la librería y el método que deseemos
-libreria23matematicas.mostrarMenu()
+numero1 = 5
+numero2 = 9
+#Llamar a la librería y el método que deseemos
+libreria23matematicas.menuCalculadora()
