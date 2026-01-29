ff027985d2d77aad806c57f1ebe12f1b5741c45d
“anasob-creator” <sobrini.ana@gmail.com>
2026-01-29 18:18:22 +0100

    cambiar

--- python24validaciones.py
+++ python24validaciones.py
@@ -1,3 +1,4 @@
+from unittest import result
 import libreria24validaciones
 print("Main de validaciones")
 print("Introduzca ISBN")
@@ -8,3 +9,9 @@ if (respuesta == True):
 else:
     print("El isbn no es correcto")
 print("Fin del programa Main")
+print("Main de DNI")
+print("Calcular letra NIF")
+print("Introduzca numero DNI")
+dni = int(input())
+respuesta=libreria24validaciones.validarDNI(dni)
+print(respuesta)
