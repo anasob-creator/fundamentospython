296c95d1a44d7c4ac343b6cff2d8ccf7a8600535
“anasob-creator” <sobrini.ana@gmail.com>
2026-02-02 20:13:27 +0100

    com

--- class31deportivo.py
+++ class31deportivo.py
@@ -2,13 +2,13 @@ from class31coche import Coche
 
 class Deportivo(Coche):
     def getVelocidadMaxima(self):
-        velocidadCoche=super.getVelocidadMaxima()
-        return velocidadCoche+100
+        velocidadCoche = super().getVelocidadMaxima()
+        return velocidadCoche + 100
     
     def turbo(self):
-        self.velocidad+=100
-        print("Turbo a tope!!!", self.velocidad)
-        
+        self.velocidad += 100
+        print("Turbo a tope!!!!", self.velocidad)
+    
     def acelerar(self):
-        self.velocidad+=45
-        print("Aceleranto en deportivo!! ", self.velocidad)
+        self.velocidad += 45
+        print("Acelerando en deportivo!!! ", self.velocidad)
