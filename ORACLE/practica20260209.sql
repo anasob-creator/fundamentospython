8f71927577e2ca3bc6e5edd546429870a951e2c7
“anasob-creator” <sobrini.ana@gmail.com>
2026-02-09 18:40:57 +0100

    subir

--- ORACLE/practica20260209.sql
+++ ORACLE/practica20260209.sql
@@ -93,3 +93,73 @@ delete from PLANTILLA where HOSPITAL_COD is NULL;
 delete from PLANTILLA where HOSPITAL_COD is NULL OR
 HOSPITAL_COD not in (select HOSPITAL_COD from HOSPITAL);
 -- Así recuperaríamos también aquellos que tengan un código que no existe en HOSPITAL
+-- POr último tenemos una instrucción para eliminar todos los registros de una tabla de la forma 
+-- más rápida, porque no tienen ROLLBACK. 
+-- TRUNCATE TABLE NOMBRETABLA
+delete from HOSPITAL;
+ROLLBACK;
+-- rollback  recupera el delete, lo que no pasaría con el truncate
+select * from HOSPITAL;
+-------------------------------------------------------------------
+- 1.  Dar de alta con fecha actual al empleado José Escriche Barrera como 
+--programador perteneciente al departamento de producción.  
+--Tendrá un salario base de 70000 pts/mes y no cobrará comisión.
+SELECT * FROM PLANTILLA;
+select * from EMP;
+(select max(EMP_NO) + 1 from EMP);
+SELECT * FROM dept;
+------ 1. 
+insert into EMP
+(APELLIDO, OFICIO, SALARIO, FECHA_ALT, COMISION, EMP_NO)
+values ('josé escriche barrera', 'PROGRAMADOR', 70000, '09/02/26', 0,
+(select max(EMP_NO) + 1 from EMP))
+where DEPT_NO = (select from DEPT where DNOMBRE='PRODUCCIÓN'); 
+-- 40
+-- 2 Se quiere dar de alta un departamento de informática situado en Fuenlabrada (Madrid).
+insert into DEPT
+(DNOMBRE, LOC, DEPT_NO)
+values ('INFORMATICA', 'FUENLABRADA (MADRID)',
+(SELECT max(DEPT_NO) + 10 FROM DEPT));
+-- 3. El departamento de ventas, por motivos peseteros, se traslada a Teruel, realizar dicha modificación.
+update DEPT set loc='TERUEL'
+where DNOMBRE='VENTAS';
+-- 4. En el departamento anterior (ventas), se dan de alta dos empleados: 
+-- Julián Romeral y Luis Alonso.  
+--Su salario base es el menor que cobre un empleado, y 
+--cobrarán una comisión del 15% de dicho salario.
+SELECT * FROM PLANTILLA;
+select * from EMP;
+(select max(EMP_NO) + 1 from EMP);
+SELECT * FROM dept;
+----------
+insert into EMP
+(EMP_NO, APELLIDO, SALARIO, COMISION, DEPT_NO)
+values
+((select max(EMP_NO) + 1 from EMP), 'Julián Romeral', 
+(select min(SALARIO) from EMP), 
+((select min(SALARIO) from EMP)*0.15),
+(select DEPT_NO from DEPT where DNOMBRE='VENTAS'));
+insert into EMP
+(EMP_NO, APELLIDO, SALARIO, COMISION, DEPT_NO)
+values
+((select max(EMP_NO) + 1 from EMP), 'Luis Alonso', 
+(select min(SALARIO) from EMP), 
+((select min(SALARIO) from EMP)*0.15),
+(select DEPT_NO from DEPT where DNOMBRE='VENTAS'));
+--- vamos a borrar los duplicados
+delete from EMP where APELLIDO='Julián Romeral'; 
+delete from EMP where APELLIDO='Luis Alonso';
+delete from EMP where APELLIDO='josé escriche barrera'; 
+-- 5. Modificar la comisión de los empleados de la empresa, 
+-- de forma que todos tengan un incremento del 10% del salario.
+update EMP set SALARIO=(SALARIO+(SALARIO*0.10));
+select * from EMP;
+---
+-- 6. Incrementar un 5% el salario 
+--de los interinos de la plantilla 
+-- que trabajen en el turno de noche.
+SELECT * FROM PLANTILLA; 
+update PLANTILLA set SALARIO=
+(select SALARIO from Plantilla where TURNO='N' AND
+where FUNCION='INTERINO';
+
