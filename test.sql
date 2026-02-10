0e4c32c90b4335d317bd395053b923dff6be5125
“anasob-creator” <sobrini.ana@gmail.com>
2026-02-10 20:17:50 +0100

    up

--- test.sql
+++ test.sql
@@ -1,13 +1,14 @@
 -- consulta de test para probar
+-- Para mirar el tipo de dato que tenemos se puede mirar
+-- en la estructura o bien más rápido es esta instrucción
+desc HOSPITAL;
 select APELLIDO, DIRECCION from ENFERMO where INSCRIPCION= 10995;
 select APELLIDO, FUNCION from PLANTILLA where TURNO='T'; 
 select * from PLANTILLA; 
 select * from ENFERMO; 
 delete from ENFERMO where INSCRIPCION=10995;
 ROLLBACK;
-
 insert into DEPT values (88, 'NUEVO', 'NUEVO');
 select * from ENFERMO; 
 select * from HOSPITAL;
 insert into HOSPITAL values (49, 'la luz', 'reina victoria','914-8747','300');
-
