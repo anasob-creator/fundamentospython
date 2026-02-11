bacb53234508dad259dcd059c28f8d57915044f1
“anasob-creator” <sobrini.ana@gmail.com>
2026-02-11 20:12:51 +0100

    up

--- python46menuplantilla.py
+++ python46menuplantilla.py
@@ -1,5 +1,10 @@
-# En esta V3 vamos a agregar los oficios de manera dinámica
-# direcctamente de la tabla en lugar de meterlos a mano
+# Quiero un menú con las funciones de la plantilla
+# El usuario seleccionará una función y mostramos 
+# los apellidos de la función seleccionada
+# 1. Interino
+# 2. Enfermera
+# 3. Enfermera
+# seleccione una opción:
 import oracledb
 connection=oracledb.connect(user="SYSTEM", password="oracle"
                             ,dsn="localhost/FREEPDB1")
@@ -7,34 +12,27 @@ cursor=connection.cursor()
 # Necesitamos una lista de oficios
 listaOficios=[]
 # Agregamos oficios
-# listaOficios.append("ANALISTA")
-# listaOficios.append("DIRECTOR")
-# listaOficios.append("PROGRAMATOR")
-# listaOficios.append("VENDEITOR")
-# listaOficios.append("SUBDIRECTOR")
-# Para mostrar un menú, recorremos la lista
-sql="select distinct OFICIO from EMP"
+sql="select distinct FUNCION from PLANTILLA"
 cursor.execute(sql)
 # recorremos y por cada oficio append
 for row in cursor:
     # Agregamaos cada oficio
     listaOficios.append(row[0])
-# Creamos un contador
+    # Creamos un contador
 contador=1
 for ofi in listaOficios:
     print(f"{contador}.- {ofi}")
     contador=contador+1
 print("Seleccione una opción: ")
 opcion=int(input())
-oficioSeleccionado=listaOficios[opcion - 1]
-print(f"Opción seleccionada: {oficioSeleccionado}")
+funcionSeleccionada=listaOficios[opcion - 1]
+print(f"Opción seleccionada: {funcionSeleccionada}")
 # Consultamos los empleados con el oficio oficioSeleccionado
-sql="select * from EMP where OFICIO=:oficio"
-cursor.execute(sql, (oficioSeleccionado,))
+sql="select * from PLANTILLA where FUNCION=:funcion"
+cursor.execute(sql, (funcionSeleccionada,))
 print("----- LISTA  EMPLEADOS -----")
 for row in cursor:
-    print(f" - {row[1]}, Salario: {row[5]}")
+    print(f" - {row[3]}, Salario: {row[6]}")
 cursor.close()
 connection.close()
 print("Fin del programa")
-
